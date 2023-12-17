53f5dee60732b2cff64c7951fc02e74293663ab0
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2023-12-17 13:34:44 +0100

    day01 cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Not;
+
 use utils::parse_text;
 
 fn main() {
@@ -9,14 +11,20 @@ fn main() {
 
     if let Some(cal_numbers) = calibration_numbers {
         let calibration_sum: u32 = cal_numbers.into_iter().sum();
-        println!("The sum of original calibration values is {}", calibration_sum);
+        println!(
+            "The sum of original calibration values is {}",
+            calibration_sum
+        );
     } else {
         println!("The sum of original calibration values is undefined");
     }
 
     if let Some(mod_cal_numbers) = mod_calibration_numbers {
         let calibration_sum: u32 = mod_cal_numbers.into_iter().sum();
-        println!("The sum of transformed calibration values is {}", calibration_sum);
+        println!(
+            "The sum of transformed calibration values is {}",
+            calibration_sum
+        );
     } else {
         println!("The sum of transformed calibration values is undefined");
     }
@@ -30,38 +38,39 @@ fn get_lines(text: &str) -> Vec<String> {
 }
 
 fn transform_numbers(lines: &Vec<String>) -> Vec<String> {
-    lines
-        .into_iter()
-        .map(|line| {
-            let mut res = Vec::new();
-            let chars = line.chars().collect::<Vec<_>>();
-            for i in 0..line.len() {
-                if chars[i].is_digit(10) {
-                    res.push(chars[i].to_digit(10).unwrap())
-                }
-                let rest: String = (&chars[i..]).iter().collect();
-                if rest.starts_with("one") {
-                    res.push(1);
-                } else if rest.starts_with("two") {
-                    res.push(2);
-                } else if rest.starts_with("three") {
-                    res.push(3);
-                } else if rest.starts_with("four") {
-                    res.push(4);
-                } else if rest.starts_with("five") {
-                    res.push(5);
-                } else if rest.starts_with("six") {
-                    res.push(6);
-                } else if rest.starts_with("seven") {
-                    res.push(7);
-                } else if rest.starts_with("eight") {
-                    res.push(8);
-                } else if rest.starts_with("nine") {
-                    res.push(9);
-                }
-            }
-            res.into_iter().filter_map(|i| char::from_digit(i, 10)).collect()
-        })
+    lines.into_iter().map(extract_digits).collect()
+}
+
+fn extract_digits(line: &String) -> String {
+    let mut res = Vec::new();
+    let chars = line.chars().collect::<Vec<_>>();
+    for i in 0..line.len() {
+        if chars[i].is_digit(10) {
+            res.push(chars[i].to_digit(10).unwrap())
+        }
+        let rest: String = (&chars[i..]).iter().collect();
+        if rest.starts_with("one") {
+            res.push(1);
+        } else if rest.starts_with("two") {
+            res.push(2);
+        } else if rest.starts_with("three") {
+            res.push(3);
+        } else if rest.starts_with("four") {
+            res.push(4);
+        } else if rest.starts_with("five") {
+            res.push(5);
+        } else if rest.starts_with("six") {
+            res.push(6);
+        } else if rest.starts_with("seven") {
+            res.push(7);
+        } else if rest.starts_with("eight") {
+            res.push(8);
+        } else if rest.starts_with("nine") {
+            res.push(9);
+        }
+    }
+    res.into_iter()
+        .filter_map(|i| char::from_digit(i, 10))
         .collect()
 }
 
@@ -74,10 +83,9 @@ fn get_calibration_number(line: &String) -> Option<u32> {
         .chars()
         .filter_map(|c| c.to_digit(10))
         .collect::<Vec<u32>>();
-    if filtered.is_empty() {
-        None
-    } else {
-        Some(10 * filtered.first().expect("First digit must exist")
-            + filtered.last().expect("Last digit must exist"))
-    }
+
+    filtered.is_empty().not().then(|| {
+        10 * filtered.first().expect("First digit must exist")
+            + filtered.last().expect("Last digit must exist")
+    })
 }
